0891b0a21c46bb4bddca2605f1e7a0a5c3ac1ada
Hanson C. <47832494+Hanson2258@users.noreply.github.com>
2026-01-19 17:40:30 -0800

    Added Approximate Camera Positions

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -22,9 +22,9 @@ public class VisionConstants {
 
   // Robot to camera transforms
   public static Transform3d robotToCamera0 =
-      new Transform3d(0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, 0.0));
+      new Transform3d(0.2794, 0.0381, 0.52705, new Rotation3d(0.0, Math.toRadians(12), 0.0));
   public static Transform3d robotToCamera1 =
-      new Transform3d(-0.2, 0.0, 0.2, new Rotation3d(0.0, -0.4, Math.PI));
+      new Transform3d(0.0, 10.0, 0.1524, new Rotation3d(0.0, 0.0, 0.0));
 
   // Basic filtering thresholds
   public static double maxAmbiguity = 0.3;
