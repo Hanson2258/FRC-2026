2db2ed55542254ca6dab8d24a6780e20f5a61002
mydimons <mydimons.kb@gmail.com>
2026-03-02 23:30:07 -0800

    Min/Max rads and motor inverted constants

--- src/main/java/frc/robot/subsystems/rotator/RotatorConstants.java
+++ src/main/java/frc/robot/subsystems/rotator/RotatorConstants.java
@@ -16,6 +16,17 @@ public class RotatorConstants {
   /** Target position when the rotator is in the DOWN mode */
   public static final double kDownRotatorRads = 0;
 
+  /** Set true to invert the motor */
+  public static final boolean kMotorInverted = false;
+
+  // TODO: tune max pos
+  /** Max radians for the rotator to rotate */
+  public static final double kMaxRads = 0.5;
+  
+  // TODO: tune min voltage
+  /** Min radians for the rotator to rotate */
+  public static final double kMinRads = 0;
+
   // TODO: tune PID
   /** PID values for to-position target */
   public static final double kP = 0;
