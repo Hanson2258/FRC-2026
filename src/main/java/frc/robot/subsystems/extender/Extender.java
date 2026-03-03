d0720276b1a524e0828ff4a69d8925a60557bdf2
mydimons <mydimons.kb@gmail.com>
2026-03-02 20:49:28 -0800

    more layout functions

--- src/main/java/frc/robot/subsystems/extender/Extender.java
+++ src/main/java/frc/robot/subsystems/extender/Extender.java
@@ -8,7 +8,35 @@ public class Extender extends SubsystemBase {
   private final ExtenderIO.ExtenderIOInputs extenderInputs = new ExtenderIO.ExtenderIOInputs();
   //private final ExtenderIO.ExtenderIOInputs inputs;
 
+  public enum ExtenderStates {
+    UP,
+    DOWN,
+    IDLE
+  }
   public Extender(ExtenderIO io) {
     extenderIO = io; 
   }
+
+  @Override
+  public void periodic() {
+
+  }
+
+  public void resetEncoders() {
+
+  }
+
+  public void setTargetPosition(double position) {
+    extenderInputs.targetPositionRads = position;
+  }
+
+  public double getTargetPosition() {
+    return extenderInputs.positionRads;
+  }
+
+  public void stepPosition(double steps) {
+
+  }
+
+
 }
--- src/main/java/frc/robot/subsystems/extender/ExtenderConstants.java
+++ src/main/java/frc/robot/subsystems/extender/ExtenderConstants.java
@@ -3,5 +3,14 @@ package frc.robot.subsystems.extender;
 import com.revrobotics.spark.config.SparkBaseConfig;
 
 public class ExtenderConstants {
-  
+
+  // TODO: tune position
+  public final double UP_EXTENDER_POSITION = 0;
+
+  // TODO: tune position
+  public final double DOWN_EXTENDER_POSITION = 0;
+
+  public final double kP = 0;
+  public final double kI = 0;
+  public final double kD = 0;
 }
