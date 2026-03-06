857b71d7e2ef2dfe825ec242375295887629c6af
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2026-03-06 10:46:00 -0800

    Added VisionConstants for Beta

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -22,8 +22,12 @@ public class VisionConstants {
   public static String camera1Name = "webcam_0";
 
   // Robot to camera transforms (index by camera index; add more entries when adding cameras)
+  // // For Alpha
+  // public static Transform3d robotToCamera0 =
+  //     new Transform3d(-0.33, 0.19, 0.435, new Rotation3d(Math.toRadians(0.98), Math.toRadians(-21.21), Math.toRadians(-178.34)));
+  // For Beta
   public static Transform3d robotToCamera0 =
-      new Transform3d(-0.2, -0.3, 0.35, new Rotation3d(Math.toRadians(4.4), Math.toRadians(27), Math.toRadians(-178.0)));
+      new Transform3d(-0.33, 0.19, 0.435, new Rotation3d(Math.toRadians(0.98), Math.toRadians(-21.21), Math.toRadians(-178.34)));
   public static Transform3d robotToCamera1 =
       new Transform3d(0.0, 10.0, 0.1524, new Rotation3d(0.0, 0.0, 0.0));
 
