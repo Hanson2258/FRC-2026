a60c30f0556a7e452a4b0399edf1be19268448fa
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2026-03-05 21:50:27 -0800

    Added estimated Velocity Multiplier

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -35,7 +35,7 @@ public final class ShooterConstants {
    * is undershooting, decrease if overshooting.
    * 0.95x for simulator
    */
-  public static final double kExitVelocityCompensationMultiplier = 1.2;
+  public static final double kExitVelocityCompensationMultiplier = 1.22;
 
   /**
    * Single flywheel at bottom: flywheel surface speed = exit velocity / this divider (e.g. 0.5
