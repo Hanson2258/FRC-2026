281396dac3f6fe112ee13ad25ca46cc5bd868cec
Hanson2258 <47832494+Hanson2258@users.noreply.github.com>
2026-03-27 13:18:00 -0700

    Added Center hang auto, and made all starting position face forward
    - All AprilTags Face Forward to allow seeing AprilTags

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -664,6 +664,7 @@ public class RobotContainer {
 
 		// Hang Commands
 		NamedCommands.registerCommand("Hang Level 1", Commands.runOnce(() -> hang.setLevel1State(), hang));
+		NamedCommands.registerCommand("Hang Half", Commands.runOnce(() -> hang.setHalfState(), hang));
 		NamedCommands.registerCommand("Hang Stored", Commands.runOnce(() -> hang.setStoredState(), hang));
 	} // End registerCommands
 
